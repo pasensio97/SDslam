fadefd1917fa160d72d0bdb9d712abe516e034d4
pasensio97 <p.asensio97@gmail.com>
2020-12-18 02:00:51 +0100

    Updated ORBextractor.h

--- src/LoopClosing.h
+++ src/LoopClosing.h
@@ -42,8 +42,8 @@ class LoopClosing {
  public:
 
   typedef std::pair<std::set<KeyFrame*>, int> ConsistentGroup;
-  typedef std::map<KeyFrame*,g2o::Sim3, std::less<KeyFrame*>,
-    Eigen::aligned_allocator<std::pair<const KeyFrame*, g2o::Sim3> > > KeyFrameAndPose;
+  typedef std::map<KeyFrame*,g2o::Sim3,std::less<KeyFrame*>,
+    Eigen::aligned_allocator<std::pair<KeyFrame* const, g2o::Sim3> > > KeyFrameAndPose;
 
  public:
   LoopClosing(Map* pMap, const bool bFixScale);
--- src/ORBextractor.h
+++ src/ORBextractor.h
@@ -27,7 +27,7 @@
 
 #include <vector>
 #include <list>
-#include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 
 namespace SD_SLAM {
 
